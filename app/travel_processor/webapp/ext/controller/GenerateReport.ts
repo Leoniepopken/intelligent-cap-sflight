a46063dc3866f011e15654767430017d6fb8ddde
Leonie <leonie.popken@tum.de>
2024-12-18 16:52:38 +0100

    adapts comments and package.json

--- app/travel_processor/webapp/ext/controller/GenerateReport.ts
+++ app/travel_processor/webapp/ext/controller/GenerateReport.ts
@@ -9,12 +9,9 @@ import MessageToast from "sap/m/MessageToast";
  * @param pageContext the context of the page on which the event was fired
  */
 export function generateReport(this: ExtensionAPI, pageContext: Context) {
-  // casting to 'any' such that ts doesn't complain about editFlow not existing
-  const oModel = (this as any).editFlow.getView().getModel();
-  // invoke action 'generateReport' of the TravelService, passing the model and context
+  // invoke action 'generateReport' of the TravelService, passing the context
   (this as any).editFlow
     .invokeAction("TravelService.generateReport", {
-      //model: oModel,
       contexts: (this as any).getSelectedContexts(),
     })
     .then((result: any) => console.log("Success", result))
