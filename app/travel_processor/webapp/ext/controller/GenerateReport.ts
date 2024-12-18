ba4f708bcf770b4b689af3d02d4ba30f5e33c7f8
Leonie <leonie.popken@tum.de>
2024-12-18 16:27:30 +0100

    invokes backend generateReport action from the frontend when clicking on respective button

--- app/travel_processor/webapp/ext/controller/GenerateReport.ts
+++ app/travel_processor/webapp/ext/controller/GenerateReport.ts
@@ -1,6 +1,6 @@
-import ExtensionAPI from 'sap/fe/core/ExtensionAPI';
-import Context from 'sap/ui/model/odata/v4/Context';
-import MessageToast from 'sap/m/MessageToast';
+import ExtensionAPI from "sap/fe/core/ExtensionAPI";
+import Context from "sap/ui/model/odata/v4/Context";
+import MessageToast from "sap/m/MessageToast";
 
 /**
  * Generated event handler.
@@ -9,5 +9,16 @@ import MessageToast from 'sap/m/MessageToast';
  * @param pageContext the context of the page on which the event was fired
  */
 export function generateReport(this: ExtensionAPI, pageContext: Context) {
-    MessageToast.show("Custom handler invoked.");
+  // casting to 'any' such that ts doesn't complain about editFlow not existing
+  const oModel = (this as any).editFlow.getView().getModel();
+  // invoke action 'generateReport' of the TravelService, passing the model and context
+  (this as any).editFlow
+    .invokeAction("TravelService.generateReport", {
+      //model: oModel,
+      contexts: (this as any).getSelectedContexts(),
+    })
+    .then((result: any) => console.log("Success", result))
+    .catch((err: any) => console.error("Error invoking action", err));
+
+  MessageToast.show("Custom handler invoked.");
 }
