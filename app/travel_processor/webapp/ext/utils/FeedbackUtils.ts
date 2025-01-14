38b5b4c84c3ed337fe649c75b7459faf74f8262f
Leonie <leonie.popken@tum.de>
2025-01-14 15:43:17 +0100

    Adapts log message

--- app/travel_processor/webapp/ext/utils/FeedbackUtils.ts
+++ app/travel_processor/webapp/ext/utils/FeedbackUtils.ts
@@ -1,3 +1,8 @@
+import { promises as fs } from "fs";
+import * as path from "path";
+
 export async function handleFeedback(sFeedback: string, sAIResponse: string) {
-  console.log("Feedback: ", sFeedback);
+  console.log("Feedback for " + sAIResponse + ": " + sFeedback);
+
+  //TODO: write feedback to file
 }
