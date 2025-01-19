dbc8ebe64190488f566ac0558b2835cd230a1ac3
Leonie <leonie.popken@tum.de>
2025-01-19 18:43:28 +0100

    invokeQueryAction() now returns the error, too

--- app/travel_processor/webapp/ext/utils/QueryUtils.ts
+++ app/travel_processor/webapp/ext/utils/QueryUtils.ts
@@ -19,8 +19,6 @@ export async function invokeQueryAction(oView: any, query: any) {
 
     return response.value;
   } catch (err) {
-    // This 'catch' is triggered if the user pressed "No" (rejected the promise),
-    // or if an error happened in the code above
-    console.log("User canceled or an error occurred:", err);
+    return err;
   }
 }
